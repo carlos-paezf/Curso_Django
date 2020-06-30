91f0799630b6dc321e4a83d9e86713f52a14f6ee
carlos-paezf <carlos.paezf@usantoto.edu.co>
2020-06-30 14:08:46 -0500

    Variables y propiedades en Plantillas

--- Proyecto1/Proyecto1/views.py
+++ Proyecto1/Proyecto1/views.py
@@ -1,15 +1,29 @@
 from django.http import HttpResponse
 import datetime
+from django.template import Template, Context
+
+class Persona(object):
+    def __init__(self, nombre, apellido):
+        self.nombre = nombre
+        self.apellido = apellido
 
 def saludo(request):    #Primera Vista
-    return HttpResponse("<html><body><h1>Mi primer programa con el Framework Django</h1></body></html>")
+    #Plantilla
+    doc_externo = open("D:/Curso_Django/Proyecto1/Proyecto1/plantillas/saludo.html")
+    plantilla = Template(doc_externo.read())
+    doc_externo.close()
+    contexto = Context()
+    documento = plantilla.render(contexto)
+    return HttpResponse(documento)
 
 def despedida(request):
     documento = '''
     <html>
         <body>
             <h1>
-                Se han agregado de manera correcta las vistas
+                Se han agregado de manera correcta las vistas. Esta es una
+                de las maneras mas rusticas y no optimas de agregar las cadenas
+                de texto
             </h1>
         </body>
     </html>'''
@@ -17,25 +31,27 @@ def despedida(request):
 
 def fecha(request):
     fecha_actual = datetime.datetime.now()
-    cuerpo = '''
-    <html>
-        <body>
-            <h1>
-                Fecha y hora actuales %s
-            </h1>
-        </body>
-    </html>''' % fecha_actual
+    doc_externo = open("D:/Curso_Django/Proyecto1/Proyecto1/plantillas/fecha.html")
+    plantilla = Template(doc_externo.read())
+    doc_externo.close()
+    contexto = Context({"fecha_actual":fecha_actual})
+    cuerpo = plantilla.render(contexto)
     return HttpResponse(cuerpo)
 
 def calcularEdad (request,edad,agno):  #Primera URL con parametro
+    #Creacion de objeto
+    persona1 = Persona("David", "Ferrer")
+    #Calculos de la edad
     periodo = agno-2020
     edadFutura = edad+periodo
-    cuerpo = '''
-    <html>
-        <body>
-            <h1>
-                En el año %s tendras %s años
-            </h1>
-        </body>
-    </html>''' %(agno,edadFutura)
+    #Plantilla
+    doc_externo = open("D:/Curso_Django/Proyecto1/Proyecto1/plantillas/calcular_edad.html")
+    plantilla = Template(doc_externo.read())
+    doc_externo.close()
+    contexto = Context({"edad":edad,
+                        "agno":agno,
+                        "edad_futura":edadFutura,
+                        "nombre":persona1.nombre,
+                        "apellido":persona1.apellido})
+    cuerpo = plantilla.render(contexto)
     return HttpResponse(cuerpo)
