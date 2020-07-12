11b145bcdb837d72b070b7cc806f7e0d2cc6c1c5
carlos-paezf <carlos.paezf@usantoto.edu.co>
2020-07-12 13:30:54 -0500

    Creacion de Views, URLs y Templates

---
+++ ProyectoWebCompleto/ProyectoGuiado/ProjectWebApp/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'ProjectWebApp/home.html')
+
+
+def services(request):
+    return render(request, 'ProjectWebApp/services.html')
+
+
+def shop(request):
+    return render(request, 'ProjectWebApp/shop.html')
+
+
+def blog(request):
+    return render(request, 'ProjectWebApp/blog.html')
+
+
+def contact(request):
+    return render(request, 'ProjectWebApp/contact.html')
