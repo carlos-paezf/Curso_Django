c695fea236fa4f03beec47b2063c195e1ae7d0e4
carlos-paezf <carlos.paezf@usantoto.edu.co>
2021-01-10 16:03:30 -0500

    Guardar Info de Contanto

--- ProyectoWebCompleto/ProyectoGuiado/Contact/views.py
+++ ProyectoWebCompleto/ProyectoGuiado/Contact/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import FormularioContacto
 
 # Create your views here.
@@ -6,4 +6,11 @@ from .forms import FormularioContacto
 
 def contact(request):
     formulario_contacto = FormularioContacto()
+    if request.method=="POST":
+        formulario_contacto = FormularioContacto(data = request.POST)
+        if formulario_contacto.is_valid():
+            nombre = request.POST.get("nombre")
+            email = request.POST.get("email")
+            contenido = request.POST.get("contenido")
+            return redirect("/contact/?OK")
     return render(request, 'contact/contact.html', {'miFormulario': formulario_contacto})
