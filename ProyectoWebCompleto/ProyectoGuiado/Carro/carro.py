d0162cab6f23753559941a2b35648b4f89802f0d
carlos-paezf <carlos.paezf@usantoto.edu.co>
2021-03-24 11:02:36 -0500

    Funciones del carro

--- ProyectoWebCompleto/ProyectoGuiado/Carro/carro.py
+++ ProyectoWebCompleto/ProyectoGuiado/Carro/carro.py
@@ -8,6 +8,7 @@ class Carro:
         else:
             self.carro = Carro
     
+    
     def add_product(self, product):
         if (str(product.id) not in self.carro.keys()):
             self.carro[product.id] = {
@@ -17,3 +18,36 @@ class Carro:
                 'amount': 1, 
                 'image': product.image.url
             }
+        else:
+            for key, value in self.carro.items():
+                if key == str(product.id):
+                    value['amount'] += 1
+                    break
+        self.save_session()
+    
+    
+    def save_session(self):
+        self.session['carro'] = self.carro
+        self.session.modified = True
+    
+    
+    def delete_product(self, product):
+        product.id = str(product.id)
+        if product.id in self.carro:
+            del self.carro[product.id]
+            self.save_session()
+    
+    
+    def substract_units_product(self, product):
+        for key, value in self.carro:
+            if key == str(product.id):
+                value['amount'] -= 1
+                if value['amount'] < 1:
+                    self.delete_product(product)
+                break
+        self.save_session()
+    
+    
+    def clean_carro(self):
+        self.session['carro'] = {}
+        self.session.modified = True
