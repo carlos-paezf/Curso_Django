7b1f32c6f78aa2115c14adbbb2e7897a606f5fc3
carlos-paezf <carlos.paezf@usantoto.edu.co>
2021-04-30 12:16:30 -0500

    Modificación al carrito de compras

--- ProyectoWebCompleto/ProyectoGuiado/ProyectoGuiado/settings.py
+++ ProyectoWebCompleto/ProyectoGuiado/ProyectoGuiado/settings.py
@@ -68,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'Carro.context_processor.total_amount_carro',
             ],
         },
     },
